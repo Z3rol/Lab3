71d9678b01358e0a435b053192b0eb276bc60d2b
Спицин Денис <spytsyn.denys1125@vu.cdu.edu.ua>
2025-11-07 12:45:59 +0200

    Програма для виведення всіх чисел Армстронга в діапазоні [2, n]

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab3
+{
+    class Program
+    {
+        //Рахує кількість цифр в n та повертає список з усіма цифрами і кількість цифр
+        static void CalcNumberOfDigits(int n, out List<int> listOfDigits, out int number)
+        {
+            n = Math.Abs(n);
+            listOfDigits = new List<int>();
+
+            if (n > 0)
+            {
+                while (n > 0)
+                {
+                    listOfDigits.Add(n % 10);
+
+                    n /= 10;
+                }
+
+                number = listOfDigits.Count;
+            }
+            else
+            {
+                listOfDigits.Add(0);
+                number = 1;
+            }
+
+        }
+        //Приводить всі цифри до заданого степеню та повертає суму всіх результатів
+        static void CalcOfDigitsPowers(List<int> listOfDigits, int number, out int digitsSum)
+        {
+            digitsSum = 0;
+
+            if (number > 1)
+            {
+                foreach (int digit in listOfDigits)
+                {
+                    digitsSum += (int)Math.Pow(digit, number);
+                }
+            }
+            else
+            {
+                foreach (int num in listOfDigits)
+                {
+                    digitsSum += num;
+                }
+            }
+        }
+        //Викликає CalcNumberOfDigits і CalcOfDigitsPowers та повертає true якщо це число Армстронга та false якщо ні
+        static void IsArmstrongNumber(int n, out bool isArmstrong)
+        {
+            int nOriginal = n;
+
+            CalcNumberOfDigits(n, out List<int> listOfDigits, out int number);
+            CalcOfDigitsPowers(listOfDigits, number, out int digitsSum);
+
+            isArmstrong = nOriginal == digitsSum;
+        }
+        //Виводить всі числа Армстронга з диапазону [2, n]
+        static void PrintAllArmstrongNumbers(int n)
+        {
+            for (int i = 2; i <= n; i++)
+            {
+                IsArmstrongNumber(i, out bool isArmstrong);
+
+                if (isArmstrong)
+                    Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть число: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Вивожу всі числа армстронга в диапазоні [2, {n}]");
+            PrintAllArmstrongNumbers(n);
+        }
+    }
+}
