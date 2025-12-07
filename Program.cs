5e5ca9e05db34a8e148ba708e551d201b4f66b36
Спицин Денис <spytsyn.denys1125@vu.cdu.edu.ua>
2025-12-07 15:39:03 +0200

    Виправив назви змінних та поправив структуру функції IsArmstrongNumber

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace Lab3
     class Program
     {
         //Рахує кількість цифр в n та повертає список з усіма цифрами і кількість цифр
-        static void CalcNumberOfDigits(int n, out List<int> listOfDigits, out int number)
+        static void CalcNumberOfDigits(int originalN, out List<int> listOfDigits, out int digitCount)
         {
-            n = Math.Abs(n);
+            int n = Math.Abs(originalN);
             listOfDigits = new List<int>();
 
             if (n > 0)
@@ -20,51 +20,41 @@ namespace Lab3
                     n /= 10;
                 }
 
-                number = listOfDigits.Count;
+                digitCount = listOfDigits.Count;
             }
             else
             {
                 listOfDigits.Add(0);
-                number = 1;
+                digitCount = 1;
             }
 
         }
         //Приводить всі цифри до заданого степеню та повертає суму всіх результатів
-        static void CalcOfDigitsPowers(List<int> listOfDigits, int number, out int digitsSum)
+        static void CalcSumOfDigitsPowers(List<int> listOfDigits, int digitsCount, out long digitsSum)
         {
             digitsSum = 0;
 
-            if (number > 1)
+            foreach (int digit in listOfDigits)
             {
-                foreach (int digit in listOfDigits)
-                {
-                    digitsSum += (int)Math.Pow(digit, number);
-                }
-            }
-            else
-            {
-                foreach (int num in listOfDigits)
-                {
-                    digitsSum += num;
-                }
+                digitsSum += (long) Math.Pow(digit, digitsCount);
             }
         }
         //Викликає CalcNumberOfDigits і CalcOfDigitsPowers та повертає true якщо це число Армстронга та false якщо ні
-        static void IsArmstrongNumber(int n, out bool isArmstrong)
+        static bool IsArmstrongNumber(int n)
         {
             int nOriginal = n;
 
             CalcNumberOfDigits(n, out List<int> listOfDigits, out int number);
-            CalcOfDigitsPowers(listOfDigits, number, out int digitsSum);
+            CalcSumOfDigitsPowers(listOfDigits, number, out long digitsSum);
 
-            isArmstrong = nOriginal == digitsSum;
+            return nOriginal == digitsSum;
         }
         //Виводить всі числа Армстронга з диапазону [2, n]
         static void PrintAllArmstrongNumbers(int n)
         {
             for (int i = 2; i <= n; i++)
             {
-                IsArmstrongNumber(i, out bool isArmstrong);
+                bool isArmstrong = IsArmstrongNumber(i);
 
                 if (isArmstrong)
                     Console.WriteLine(i);
@@ -82,22 +72,17 @@ namespace Lab3
 
                 if (int.TryParse(Console.ReadLine(), out int n))
                 {
-                    bool validNumber = false;
-
-                    do
+                    if (n >= 2)
                     {
-                        if (n >= 2)
-                        {
-                            Console.WriteLine($"Вивожу всі числа армстронга в диапазоні [2, {n}]");
-                            PrintAllArmstrongNumbers(n);
+                        Console.WriteLine($"Вивожу всі числа армстронга в диапазоні [2, {n}]");
+                        PrintAllArmstrongNumbers(n);
 
-                            isRunning = false;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Число повинне бути не меньше ніж 2");
-                        }
-                    } while (validNumber = false);
+                        isRunning = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Число не може бути менше ніж 2");
+                    }
                 }
                 else
                 {
